79b3e0d84d5a95299b5aa6865c93fe12110bd2ba
AleksandrVasilko <avasilko7@gmail.com>
2022-04-23 16:06:32 +0300

    add component HomePage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
 import { ToastContainer } from "react-toastify";
-//import { Suspense, lasy } from "react";
-//import { Route, Routes } from "react-router-dom";
-import Navigation from './Navigation/Navigation'
+import { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navigation from './Navigation/Navigation';
+
+const HomePage = lazy(() =>
+  import('../views/HomePage')
+);
 
 function App() { 
   return (
     <section>
       <ToastContainer />
-      <Navigation/>
+      <Navigation />
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route exapt path='/' element={<HomePage />} />
+        </Routes>
+      </Suspense>
     </section>
   )
 }
